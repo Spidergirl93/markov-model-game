adaadfe0930b9f4e85b79e6952c67d8cfbae9bac
Maryam <71137692+Spidergirl93@users.noreply.github.com>
2023-01-09 20:53:01 +0100

    a basic markov model generator based on text chars is added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,14 +16,28 @@ const Home = (props) => {
     let ngrams = {};
     for (let i = 0; i <= txt.length - order; i++) {
       let gram = txt.substring(i, i + order);
-    
+
       if (!ngrams[gram]) {
         ngrams[gram] = [];
       }
+      //Duplicates here are in our favor since the more an item is, the higher chance it has to get picked
       ngrams[gram].push(txt.charAt(i + order));
     }
-    console.log(ngrams);
-    return "nothing for now";
+    let currentGram = txt.substring(0, order);
+    let result = currentGram;
+
+    // Do this 10 times to get a longer text as the result
+    for (let i = 0; i < 1000; i++) {
+      // Get the possible next states
+      let possibilities = ngrams[currentGram];
+      // Pick a random element from the possible next transitions
+      let index = Math.floor(Math.random() * possibilities.length);
+      let next = possibilities[index];
+      result += next;
+      currentGram = result.substring(result.length - order);
+    }
+
+    return result;
   };
 
   return (
